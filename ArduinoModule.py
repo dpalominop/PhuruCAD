3a3dadf86a2016c69ae5f7790dbcbcdd74763744
Daniel Palomino <dpalominop07@gmail.com>
2015-07-17 20:20:30 -0500

    Se ha creado un comando para ejecutar la recolección de puentos del puerto serial sobre usb. Falta acomodar los nombres, crear un boton de parada y mejorar el dibujado de las líneas.

---
+++ ArduinoModule.py
@@ -0,0 +1,74 @@
+import FreeCAD as App
+import FreeCADGui as Gui
+import Part,PartGui
+
+class MyTool:
+    "My tool object"
+    from PySide import QtGui,QtCore
+    timer = QtCore.QTimer()
+
+    def GetResources(self):
+        return {"MenuText": "My Command",
+                       "Accel": "Ctrl+M",
+                       "ToolTip": "Comando de DANIEL",
+                       "Pixmap"  : """
+            /* XPM */
+            static const char *test_icon[]={
+            "16 16 2 1",
+            "a c #000000",
+            ". c None",
+            "................",
+            "................",
+            "..############..",
+            "..############..",
+            "..############..",
+            "......####......",
+            "......####......",
+            "......####......",
+            "......####......",
+            "......####......",
+            "......####......",
+            "......####......",
+            "......####......",
+            "......####......",
+            "................",
+            "................"};
+            """}
+
+    def IsActive(self):
+        return True
+        #if FreeCAD.ActiveDocument == None:
+        #    return False
+        #else:
+        #    return True
+
+    def Activated(self):
+        # do something here...
+        print "Activando comando"
+        self.iniciarProceso()
+        
+    def iniciarProceso(self):
+        import serial
+        #from PySide import QtGui,QtCore
+        
+        doc = App.newDocument("PHURU")
+        l = Part.Line()
+        l.StartPoint = App.Vector(0.0,0.0,0.0)
+        
+        PuertoSerie = serial.Serial('/dev/ttyUSB0', 9600)
+        
+        def dibujarPunto():
+            sDatos = PuertoSerie.readline()
+            x,y,z,d = sDatos.split(",")
+            
+            l.EndPoint = l.EndPoint.add(App.Vector(float(x),float(y),float(z)))
+            
+            doc.addObject("Part::Feature","Line").Shape = l.toShape() 
+            doc.recompute()
+            l.StartPoint = l.EndPoint.add(App.Vector(0.0,0.0,0.0))
+
+        #timer = QtCore.QTimer()
+        self.timer.timeout.connect(dibujarPunto)
+        self.timer.start(1000)
+
+Gui.addCommand('ArduinoCAD', MyTool())
--- InitGui.py
+++ InitGui.py
@@ -1,28 +1,43 @@
 # Phuru gui init module
-# (c) 2001 Juergen Riegel LGPL
-
-import FreeCAD as App
-import FreeCADGui as Gui
-import Part,PartGui
+# (c) 2001 Juergen Riegel LGPL¿
 
 class PhuruWorkbench ( Workbench ):
-	from PySide import QtGui,QtCore
 	"Phuru workbench object"
+	Icon = """
+			/* XPM */
+			static const char *test_icon[]={
+			"16 16 2 1",
+			"a c #000000",
+			". c None",
+			"................",
+			"................",
+			"..############..",
+			"..############..",
+			"..############..",
+			"......####......",
+			"......####......",
+			"......####......",
+			"......####......",
+			"......####......",
+			"......####......",
+			"......####......",
+			"......####......",
+			"......####......",
+			"................",
+			"................"};
+			"""
 	MenuText = "Phuru"
 	ToolTip = "Phuru workbench"
-	timer = QtCore.QTimer()
-	#doc = App.newDocument("PHURU")
-	#l = Part.Line()
-	
+
 	def Initialize(self):
-		# load the module
-		Msg ("Iniciando ...\n")
-		
-		self.iniciarProceso()
-		
+		import ArduinoModule
+		self.appendToolbar("My Tools", ["ArduinoCAD"])
+		self.appendMenu("My Tools", ["ArduinoCAD"])
+
 	def GetClassName(self):
-		return "PhuruGui::Workbench"
-	
+		#return "PhuruGui::Workbench"
+		return "Gui::PythonWorkbench"
+
 	def Activated(self):
 		# do something here if needed...
 		Msg ("PhuruWorkbench.Activated()\n")
@@ -30,30 +45,5 @@ class PhuruWorkbench ( Workbench ):
 	def Deactivated(self):
 		# do something here if needed...
 		Msg ("PhuruWorkbench.Deactivated()\n")
-	
-	def iniciarProceso(self):
-		import serial
-		#from PySide import QtGui,QtCore
-		
-		doc = App.newDocument("PHURU")
-		l = Part.Line()
-		l.StartPoint = App.Vector(0.0,0.0,0.0)
-		
-		PuertoSerie = serial.Serial('/dev/ttyUSB0', 9600)
-		
-		def dibujarPunto():
-			sDatos = PuertoSerie.readline()
-			x,y,z,d = sDatos.split(",")
-			
-			l.EndPoint = l.EndPoint.add(App.Vector(float(x),float(y),float(z)))
-			
-			doc.addObject("Part::Feature","Line").Shape = l.toShape() 
-			doc.recompute()
-			l.StartPoint = l.EndPoint.add(App.Vector(0.0,0.0,0.0))
 
-		#timer = QtCore.QTimer()
-		self.timer.timeout.connect(dibujarPunto)
-		self.timer.start(1000)
-		
-
-Gui.addWorkbench(PhuruWorkbench())
+FreeCADGui.addWorkbench(PhuruWorkbench)
