0df5117b8c367b8dd1ddb0349bb5941f818468ca
Daniel Palomino <dapalominop@gmail.com>
2015-11-05 00:07:48 -0500

    Creación de hexaedro ajustado a la pantalla.

--- Calibracion/PhGyroscope.py
+++ Calibracion/PhGyroscope.py
@@ -7,15 +7,16 @@ Created on 28/9/2015
 from __future__ import division # allows floating point division from integers
 import FreeCAD, Part
 from FreeCAD import Base
+from wx import Height
 
 class PhGyroscope:
     def __init__(self, obj):
         ''' Add the properties: Radius, Eccentricity, Height, Segments (see Property View) '''
-        obj.addProperty("App::PropertyLength","Radius","Gyroscope","Base radius").Radius=60.0
-        obj.addProperty("App::PropertyLength","Eccentricity","Gyroscope","Rotor eccentricity").Eccentricity=12.0
-        obj.addProperty("App::PropertyLength","Height","Gyroscope","Height of the block").Height=30.0
-        obj.addProperty("App::PropertyLength","Segments","Gyroscope","Number of the line segments").Segments=72
-        obj.addProperty("App::PropertyLength","Twenty","Gyroscope","Twenty").Twenty=20.0
+        #obj.addProperty("App::PropertyLength","Radius","Gyroscope","Base radius").Radius=60.0
+        #obj.addProperty("App::PropertyLength","Eccentricity","Gyroscope","Rotor eccentricity").Eccentricity=12.0
+        obj.addProperty("App::PropertyLength","Height","Gyroscope","Height of the block").Height=10.0
+        #obj.addProperty("App::PropertyLength","Segments","Gyroscope","Number of the line segments").Segments=72
+        #obj.addProperty("App::PropertyLength","Twenty","Gyroscope","Twenty").Twenty=20.0
         obj.Proxy = self
 
     def onChanged(self, fp, prop): 
@@ -24,12 +25,15 @@ class PhGyroscope:
 
     def execute(self, fp): #main part of script
         #steps=int(fp.Segments) #get value from property
-        e=fp.Eccentricity
-        r=fp.Radius
+        #e=fp.Eccentricity
+        #r=fp.Radius
         h=fp.Height
         #halfw=r+e+20
-        halfw = int(r)+int(e)+20
-        halfh=int(r)+int(e)+20
+        #halfw = int(r)+int(e)+20
+        #halfh=int(r)+int(e)+20
+        
+        halfw = 20
+        halfh = 20
         
         edge1 = Part.makeLine((-halfw,halfh,0), (halfw,halfh,0)) #lines needed to create rectangle
         edge2 = Part.makeLine((halfw,halfh,0), (halfw,-halfh,0))
@@ -48,6 +52,13 @@ def makeReferenceSystem():
     #gyros.Label = "Gyroscope"
     PhGyroscope(gyros)
     gyros.ViewObject.Proxy=0
+    
+    
+    import FreeCADGui as Gui
+    Gui.activeDocument().activeView().viewAxometric()
+    Gui.SendMsgToActiveView("ViewFit")
 
 if __name__ == "__main__": #feature will be generated after macro execution
     makeReferenceSystem()
+    import Draft
+    Draft.rotate(FreeCAD.ActiveDocument.ActiveObject,45)
