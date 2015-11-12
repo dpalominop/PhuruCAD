b3e7b2f7fa7f0b403ea5cc434ff0b7e79e630dc5
Daniel Palomino <dapalominop@gmail.com>
2015-11-12 00:31:58 -0500

    Uso de quaterniones en lugar de angulos de euler.

--- Calibracion/PhControlOrientacion.py
+++ Calibracion/PhControlOrientacion.py
@@ -9,6 +9,7 @@ from PySide import QtCore
 import FreeCADGui as Gui
 import FreeCAD as App
 import Part, Draft
+import math
 
 from Socket.PhCliente import *
 #from Calibracion.PhGiroscopo import *
@@ -66,16 +67,32 @@ class PhControlOrientacion(QtCore.QObject):
         self.timer.killTimer()
     
     def controlGiro(self):
-        rmsg = self.socket.sendCommand(1, 8, "CUCHAROS")
+        rmsg = self.socket.sendCommand(1, 7, "CUCHAROS")
         
         if rmsg["rsucces"]:
-            yaw,pitch,roll = rmsg["rdata"]
+            #yaw,pitch,roll = rmsg["rdata"]
+            q0, q1, q2, q3 = rmsg["rdata"]
+            yaw,pitch,roll = self.quat2Euler(q0, q1, q2, q3)
             
             App.getDocument("PhGyroscope").Box.Placement=App.Placement(App.Vector(0,0,0), 
                                                                          App.Rotation(yaw, pitch, roll),
                                                                          App.Vector(10,10,10))
             #App.ActiveDocument.recompute()
             App.Console.PrintMessage("rdata: " + str(rmsg["rdata"]) + "\n")
+            
+    def quat2Euler(self, q0, q1, q2, q3):
+        
+        R11 = 2.0 * q0 * q0 - 1.0 + 2.0 * q1 * q1
+        R21 = 2.0 * (q1 * q2 + q0 * q3)
+        R31 = 2.0 * (q1 * q3 - q0 * q2)
+        R32 = 2.0 * (q2 * q3 + q0 * q1)
+        R33 = 2.0 * q0 * q0 - 1.0 + 2.0 * q3 * q3
+    
+        yaw = math.atan2(R32, R33)
+        pitch = -math.atan(R31 * ((1 - R31 * R31)**-1/2))
+        roll = math.atan2(R21, R11)
+        
+        return (yaw, pitch, roll)
         
 Gui.addCommand('GYROSCOPE_1', PhControlOrientacion())
 
