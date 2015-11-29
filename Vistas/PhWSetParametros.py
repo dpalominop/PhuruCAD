ef1dbca3438a453f265dd6f0b3e876c5a0fc9d14
Daniel Palomino <dapalominop@gmail.com>
2015-11-29 17:34:37 -0500

    Actualización de vista de ventana de trabajo. Falta funcionalidad.

--- Vistas/PhWSetParametros.py
+++ Vistas/PhWSetParametros.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'PhWSetParametros.ui'
 #
-# Created: Thu Nov 12 01:06:40 2015
+# Created: Sun Nov 29 17:16:47 2015
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -11,40 +11,46 @@ from PySide import QtCore, QtGui
 
 class PhWSetParametros(QtGui.QWidget):
     windowFinished = QtCore.Signal()
+    
     def __init__(self):
         super(PhWSetParametros, self).__init__()
         self.setupUi()
         self.show()
-    
+
     def setupUi(self):
         self.setObjectName("Form")
-        #Form.setObjectName("Form")
-        self.resize(253, 320)
-        #Form.resize(253, 320)
+        self.resize(317, 309)
         self.SetMagnetometro = QtGui.QPushButton(self)
-        self.SetMagnetometro.setGeometry(QtCore.QRect(30, 80, 191, 27))
+        self.SetMagnetometro.setGeometry(QtCore.QRect(30, 30, 251, 27))
         self.SetMagnetometro.setObjectName("SetMagnetometro")
         self.SetAcelerometro = QtGui.QPushButton(self)
-        self.SetAcelerometro.setGeometry(QtCore.QRect(28, 140, 191, 27))
+        self.SetAcelerometro.setGeometry(QtCore.QRect(30, 100, 251, 27))
         self.SetAcelerometro.setObjectName("SetAcelerometro")
         self.SetGiroscopo = QtGui.QPushButton(self)
-        self.SetGiroscopo.setGeometry(QtCore.QRect(30, 200, 191, 27))
+        self.SetGiroscopo.setGeometry(QtCore.QRect(30, 160, 251, 27))
         self.SetGiroscopo.setObjectName("SetGiroscopo")
         self.Salir = QtGui.QPushButton(self)
-        self.Salir.setGeometry(QtCore.QRect(160, 290, 89, 27))
+        self.Salir.setGeometry(QtCore.QRect(190, 270, 89, 27))
         self.Salir.setObjectName("Salir")
+        self.Detener = QtGui.QPushButton(self)
+        self.Detener.setGeometry(QtCore.QRect(160, 220, 121, 27))
+        self.Detener.setObjectName("Detener")
+        self.Iniciar = QtGui.QPushButton(self)
+        self.Iniciar.setGeometry(QtCore.QRect(28, 220, 121, 27))
+        self.Iniciar.setObjectName("Iniciar")
 
         self.retranslateUi()
         #QtCore.QMetaObject.connectSlotsByName(Form)
         QtCore.QObject.connect(self.Salir, QtCore.SIGNAL("pressed()"), self.cancelar)
 
     def retranslateUi(self):
-        #Form.setWindowTitle(QtGui.QApplication.translate("Form", "CALIBRAR SENSORES", None, QtGui.QApplication.UnicodeUTF8))
         self.setWindowTitle(QtGui.QApplication.translate("Form", "CALIBRAR SENSORES", None, QtGui.QApplication.UnicodeUTF8))
         self.SetMagnetometro.setText(QtGui.QApplication.translate("Form", "CALIBRAR MAGNETÓMETRO", None, QtGui.QApplication.UnicodeUTF8))
         self.SetAcelerometro.setText(QtGui.QApplication.translate("Form", "CALIBRAR ACELERÓMETRO", None, QtGui.QApplication.UnicodeUTF8))
         self.SetGiroscopo.setText(QtGui.QApplication.translate("Form", "CALIBRAR GIRÓSCOPO", None, QtGui.QApplication.UnicodeUTF8))
         self.Salir.setText(QtGui.QApplication.translate("Form", "SALIR", None, QtGui.QApplication.UnicodeUTF8))
+        self.Detener.setText(QtGui.QApplication.translate("Form", "DETENER", None, QtGui.QApplication.UnicodeUTF8))
+        self.Iniciar.setText(QtGui.QApplication.translate("Form", "INICIAR", None, QtGui.QApplication.UnicodeUTF8))
 
     def cancelar(self):
         self.close()
