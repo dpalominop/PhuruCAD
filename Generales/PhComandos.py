a2fc757bf9f57e89bf90892944dc15a42cc7fa66
Daniel Palomino <dapalominop@gmail.com>
2015-12-19 18:32:20 -0500

    Se ha creado un nuevo boton para enviar un comando nuevo no definido anteriormente.

--- Generales/PhComandos.py
+++ Generales/PhComandos.py
@@ -28,7 +28,6 @@ class PhComandos(QtCore.QObject):
     
     def Activated(self):
         self.wComandos = PhWComandos()
-        #self.wComandos.Pausar.setEnabled(False)
         QtCore.QObject.connect(self.wComandos.Reset, 
                                QtCore.SIGNAL("pressed()"), 
                                self, 
@@ -59,7 +58,7 @@ class PhComandos(QtCore.QObject):
                                self, 
                                QtCore.SLOT("doLeerEuler()"))
         
-        QtCore.QObject.connect(self.wComandos.Quaterniobn, 
+        QtCore.QObject.connect(self.wComandos.Quaternion, 
                                QtCore.SIGNAL("pressed()"), 
                                self, 
                                QtCore.SLOT("doQuaternion()"))
@@ -89,82 +88,155 @@ class PhComandos(QtCore.QObject):
                                self, 
                                QtCore.SLOT("doCalGiros()"))
         
+        QtCore.QObject.connect(self.wComandos.Extra, 
+                               QtCore.SIGNAL("pressed()"), 
+                               self, 
+                               QtCore.SLOT("doExtra()"))
 
+        QtCore.QObject.connect(self.wComandos.Habilitar, 
+                               QtCore.SIGNAL("stateChanged()"), 
+                               self, 
+                               QtCore.SLOT("doHabilitar()"))
+        
+        #self.wComandos.Extra.setEnabled(False)
+        #self.wComandos.Valor.setEnabled(False)
+        
     @QtCore.Slot()
     def doReset(self):
+        self.activarBotones(False)
         self.socket = PhCliente()
         rmsg = self.socket.sendCommand(1, 1, "")
         self.wComandos.Respuesta.document().setPlainText(str(rmsg))
         self.socket.deleteLater()
+        self.activarBotones(True)
     
     @QtCore.Slot()
     def doVersion(self):
+        self.activarBotones(False)
         self.socket = PhCliente()
         rmsg = self.socket.sendCommand(1, 2, "")
         self.wComandos.Respuesta.document().setPlainText(str(rmsg))
         self.socket.deleteLater()
+        self.activarBotones(True)
     
     @QtCore.Slot()
     def doACK(self):
+        self.activarBotones(False)
         self.socket = PhCliente()
         rmsg = self.socket.sendCommand(1, 3, "")
         self.wComandos.Respuesta.document().setPlainText(str(rmsg))
         self.socket.deleteLater()
+        self.activarBotones(True)
     
     @QtCore.Slot()
     def doError(self):
+        self.activarBotones(False)
         self.socket = PhCliente()
         rmsg = self.socket.sendCommand(1, 12, "")
         self.wComandos.Respuesta.document().setPlainText(str(rmsg))
         self.socket.deleteLater()
+        self.activarBotones(True)
     
     @QtCore.Slot()
     def doLeerSensores(self):
+        self.activarBotones(False)
         self.socket = PhCliente()
         rmsg = self.socket.sendCommand(1, 5, "")
         self.wComandos.Respuesta.document().setPlainText(str(rmsg))
         self.socket.deleteLater()
+        self.activarBotones(True)
     
     @QtCore.Slot()
     def doLeerEuler(self):
+        self.activarBotones(False)
         self.socket = PhCliente()
         rmsg = self.socket.sendCommand(1, 7, "")
         self.wComandos.Respuesta.document().setPlainText(str(rmsg))
         self.socket.deleteLater()
+        self.activarBotones(True)
     
     @QtCore.Slot()
     def doQuaternion(self):
+        self.activarBotones(False)
         self.socket = PhCliente()
         rmsg = self.socket.sendCommand(1, 6, "")
         self.wComandos.Respuesta.document().setPlainText(str(rmsg))
         self.socket.deleteLater()
+        self.activarBotones(True)
     
     @QtCore.Slot()
     def doLeerPosicion(self):
+        self.activarBotones(False)
         self.socket = PhCliente()
         rmsg = self.socket.sendCommand(1, 11, "")
         self.wComandos.Respuesta.document().setPlainText(str(rmsg))
         self.socket.deleteLater()
+        self.activarBotones(True)
     
     @QtCore.Slot()
     def doLeerVelocidad(self):
+        self.activarBotones(False)
         self.socket = PhCliente()
         rmsg = self.socket.sendCommand(1, 4, "")
         self.wComandos.Respuesta.document().setPlainText(str(rmsg))
         self.socket.deleteLater()
+        self.activarBotones(True)
     
     @QtCore.Slot()
     def doCalMag(self):
-        pass
-    
+        self.activarBotones(False)
+        self.activarBotones(True)
+        
     @QtCore.Slot()
     def doCalAcel(self):
-        pass
+        self.activarBotones(False)
+        self.activarBotones(True)
     
     @QtCore.Slot()
     def doCalGiros(self):
-        pass
+        self.activarBotones(False)
+        self.activarBotones(True)
+        
+    @QtCore.Slot()
+    def doExtra(self):
+        var = self.wComandos.Valor.text()
+        App.Console.PrintMessage(var)
+        self.activarBotones(False)
+        self.socket = PhCliente()
+        rmsg = self.socket.sendCommand(1, int(var), "")
+        self.wComandos.Respuesta.document().setPlainText(str(rmsg))
+        self.socket.deleteLater()
+        self.activarBotones(True)
+        
+    
+    @QtCore.Slot(int)
+    def doHablitar(self, val):
+        if val == 0:
+            self.wComandos.Extra.setEnabled(False)
+            self.wComandos.Valor.setEnabled(False)
+        else:
+            self.wComandos.Extra.setEnabled(True)
+            self.wComandos.Valor.setEnabled(True)
 
+    def activarBotones(self, val):
+        self.wComandos.Reset.setEnabled(val)
+        self.wComandos.Version.setEnabled(val)
+        self.wComandos.ACK.setEnabled(val)
+        self.wComandos.Error.setEnabled(val)
+        
+        self.wComandos.Sensores.setEnabled(val)
+        self.wComandos.Euler.setEnabled(val)
+        self.wComandos.Quaternion.setEnabled(val)
+        self.wComandos.Posicion.setEnabled(val)
+        self.wComandos.Velocidad.setEnabled(val)
+        
+        self.wComandos.Magnetometro.setEnabled(val)
+        self.wComandos.Acelerometro.setEnabled(val)
+        self.wComandos.Giroscopo.setEnabled(val)
+        
+        self.wComandos.Habilitar.setEnabled(val)
+        self.wComandos.Extra.setEnabled(val)
+        self.wComandos.Valor.setEnabled(val)
         
 Gui.addCommand('COMANDOS', PhComandos())
 
